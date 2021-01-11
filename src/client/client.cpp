9fb3ed251de5f3353d1fcb4bad34cb0d15fbad4a
heelim <heelim217@gmail.com>
2021-01-11 19:29:50 +0900

    make common header(include/common.h)

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,16 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h> 
-#include <dirent.h>
-#include <pthread.h>
-
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-#define BUFSIZE 1024
+#include "common.h"
 
 int main(int argc, char* argv[]) {
 	char ip_addr[] = "127.0.0.1"; //server ip addr
